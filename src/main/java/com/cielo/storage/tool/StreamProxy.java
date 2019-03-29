7b61d954ca2044a0368e75553e3f3e52e80d7732
CieloSun <cielosun@outlook.com>
2019-03-29 20:27:46 +0800

    fix

--- src/main/java/com/cielo/storage/tool/StreamProxy.java
+++ src/main/java/com/cielo/storage/tool/StreamProxy.java
@@ -7,7 +7,6 @@ public class StreamProxy {
     private static final int PARALLEL_SIZE = 10000;
 
     public static <T> Stream<T> stream(Collection<T> collection) {
-        if (collection.size() < PARALLEL_SIZE) return collection.stream();
-        return collection.parallelStream();
+        return collection.size() < PARALLEL_SIZE ? collection.stream() : collection.parallelStream();
     }
 }
